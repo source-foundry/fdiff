5df4855f90405f4628da6f6ca2d38bf6beca7d20
Chris Simpkins <git.simpkins@gmail.com>
2019-09-05 22:53:21 -0400

    add main module tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,121 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 import pytest
 
+from fdiff.__main__ import run
 
-def test_template():
-    return True
+ROBOTO_BEFORE_PATH = os.path.join("tests", "testfiles", "Roboto-Regular.subset1.ttf")
+ROBOTO_AFTER_PATH = os.path.join("tests", "testfiles", "Roboto-Regular.subset2.ttf")
+ROBOTO_UDIFF_EXPECTED_PATH = os.path.join("tests", "testfiles", "roboto_udiff_expected.txt")
+ROBOTO_UDIFF_COLOR_EXPECTED_PATH = os.path.join("tests", "testfiles", "roboto_udiff_color_expected.txt")
+ROBOTO_UDIFF_1CONTEXT_EXPECTED_PATH = os.path.join("tests", "testfiles", "roboto_udiff_1context_expected.txt")
+
+
+# Setup: define the expected diff text for unified diff
+with open(ROBOTO_UDIFF_EXPECTED_PATH, "r") as robo_udiff:
+    ROBOTO_UDIFF_EXPECTED = robo_udiff.read()
+
+# Setup: define the expected diff text for unified color diff
+with open(ROBOTO_UDIFF_COLOR_EXPECTED_PATH, "r") as robo_udiff_color:
+    ROBOTO_UDIFF_COLOR_EXPECTED = robo_udiff_color.read()
+
+
+# Setup: define the expected diff text for unified color diff
+with open(ROBOTO_UDIFF_1CONTEXT_EXPECTED_PATH, "r") as robo_udiff_contextlines:
+    ROBOTO_UDIFF_1CONTEXT_EXPECTED = robo_udiff_contextlines.read()
+
+#
+# File path validations tests
+#
+
+
+def test_main_filepath_validations_false_firstfont(capsys):
+    test_path = os.path.join("tests", "testfiles", "bogus-font.ttf")
+    args = [test_path, test_path]
+
+    with pytest.raises(SystemExit) as exit_info:
+        run(args)
+        captured = capsys.readouterr()
+        assert captured.error.startswith("[*] ERROR: The file path")
+        assert exit_info.value.code == 1
+
+
+def test_main_filepath_validations_false_secondfont(capsys):
+    test_path_2 = os.path.join("tests", "testfiles", "bogus-font.ttf")
+    args = [ROBOTO_BEFORE_PATH, test_path_2]
+
+    with pytest.raises(SystemExit) as exit_info:
+        run(args)
+        captured = capsys.readouterr()
+        assert captured.error.startswith("[*] ERROR: The file path")
+        assert exit_info.value.code == 1
+
+
+#
+#  Unified diff integration tests
+#
+
+def test_main_run_unified_default(capsys):
+    args = [ROBOTO_BEFORE_PATH, ROBOTO_AFTER_PATH]
+
+    run(args)
+    captured = capsys.readouterr()
+
+    res_string_list = captured.out.split("\n")
+    expected_string_list = ROBOTO_UDIFF_EXPECTED.split("\n")
+
+    # have to handle the tests for the top two file path lines
+    # differently than the rest of the comparisons because
+    # the time is defined using local platform settings
+    # which makes tests fail on different remote CI testing services
+    for x, line in enumerate(res_string_list):
+        # treat top two lines of the diff as comparison of first 10 chars only
+        if x in (0, 1):
+            assert line[0:9] == expected_string_list[x][0:9]
+        else:
+            assert line == expected_string_list[x]
+
+
+def test_main_run_unified_color(capsys):
+    args = ["-c", ROBOTO_BEFORE_PATH, ROBOTO_AFTER_PATH]
+
+    run(args)
+    captured = capsys.readouterr()
+
+    res_string_list = captured.out.split("\n")
+    expected_string_list = ROBOTO_UDIFF_COLOR_EXPECTED.split("\n")
+
+    # have to handle the tests for the top two file path lines
+    # differently than the rest of the comparisons because
+    # the time is defined using local platform settings
+    # which makes tests fail on different remote CI testing services
+    for x, line in enumerate(res_string_list):
+        # treat top two lines of the diff as comparison of first 10 chars only
+        if x in (0, 1):
+            assert line[0:9] == expected_string_list[x][0:9]
+        else:
+            assert line == expected_string_list[x]
+
+
+def test_main_run_unified_context_lines_1(capsys):
+    args = ["-l", "1", ROBOTO_BEFORE_PATH, ROBOTO_AFTER_PATH]
+
+    run(args)
+    captured = capsys.readouterr()
+
+    res_string_list = captured.out.split("\n")
+    expected_string_list = ROBOTO_UDIFF_1CONTEXT_EXPECTED.split("\n")
+
+    # have to handle the tests for the top two file path lines
+    # differently than the rest of the comparisons because
+    # the time is defined using local platform settings
+    # which makes tests fail on different remote CI testing services
+    for x, line in enumerate(res_string_list):
+        # treat top two lines of the diff as comparison of first 10 chars only
+        if x in (0, 1):
+            assert line[0:9] == expected_string_list[x][0:9]
+        else:
+            assert line == expected_string_list[x]
