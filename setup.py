989a59173c21c0476bcb98430461ab5a4b802c3a
Chris Simpkins <git.simpkins@gmail.com>
2019-09-16 00:10:38 -0400

    [setup.py] add new pytest-asyncio dependency to [dev] dependencies install target

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ INSTALL_REQUIRES = [
 # Optional packages
 EXTRAS_REQUIRES = {
     # for developer installs
-    "dev": ["coverage", "pytest", "tox", "flake8", "pytype"],
+    "dev": ["coverage", "pytest", "pytest-asyncio", "tox", "flake8", "pytype"],
     # for maintainer installs
     "maintain": ["wheel", "setuptools", "twine"],
 }
